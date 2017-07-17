69f1df22a0ff26c4605254d9d3d446ac69e952a2
zihao <crazy.zihao@gmail.com>
2017-07-17 15:54:54 +0800

    1.修改应用工具类(AppUtils.java)内容； 2.新增文件工具类(FileUtils.java)。

--- app/src/main/java/crazy/zihao/androidutil/util/AppUtils.java
+++ app/src/main/java/crazy/zihao/androidutil/util/AppUtils.java
@@ -3,6 +3,9 @@ package crazy.zihao.androidutil.util;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Environment;
+
+import java.io.File;
 
 /**
  * ClassName：AppUtils
@@ -51,6 +54,95 @@ public class AppUtils {
         } catch (Exception e) {
             e.printStackTrace();
             return 1;
+        }
+    }
+
+    /**
+     * 清除本应用内部缓存(/data/data/com.xxx.xxx/cache)
+     *
+     * @param context Context
+     */
+    public static void cleanInternalCache(Context context) {
+        FileUtils.deleteFilesByDirectory(context.getCacheDir());
+    }
+
+    /**
+     * 清除本应用所有数据库(/data/data/com.xxx.xxx/databases)
+     *
+     * @param context Context
+     */
+    public static void cleanDatabases(Context context) {
+        FileUtils.deleteFilesByDirectory(new File("/data/data/"
+                + context.getPackageName() + "/databases"));
+    }
+
+    /**
+     * 清除本应用SharedPreference(/data/data/com.xxx.xxx/shared_prefs)
+     *
+     * @param context Context
+     */
+    public static void cleanSharedPreference(Context context) {
+        FileUtils.deleteFilesByDirectory(new File("/data/data/"
+                + context.getPackageName() + "/shared_prefs"));
+    }
+
+    /**
+     * 按名字清除本应用数据库
+     *
+     * @param context Context
+     * @param dbName  数据库名称
+     */
+    public static void cleanDatabaseByName(Context context, String dbName) {
+        context.deleteDatabase(dbName);
+    }
+
+    /**
+     * 清除本应用files(/data/data/com.xxx.xxx/files)下的内容
+     *
+     * @param context Context
+     */
+    public static void cleanFiles(Context context) {
+        FileUtils.deleteFilesByDirectory(context.getFilesDir());
+    }
+
+    /**
+     * 清除外部cache下的内容(/mnt/sdcard/android/data/com.xxx.xxx/cache)
+     *
+     * @param context Context
+     */
+    public static void cleanExternalCache(Context context) {
+        if (Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            FileUtils.deleteFilesByDirectory(context.getExternalCacheDir());
+        }
+    }
+
+    /**
+     * 清除自定义路径下的文件，使用需小心，请不要误删。而且只支持目录下的文件删除
+     *
+     * @param filePath 文件路径
+     */
+    public static void cleanCustomFile(String filePath) {
+        FileUtils.deleteFilesByDirectory(new File(filePath));
+    }
+
+    /**
+     * 清除本应用所有的数据
+     *
+     * @param context  Context
+     * @param filepath 待清除的文件路径
+     */
+    public static void cleanApplicationData(Context context, String... filepath) {
+        cleanInternalCache(context);
+        cleanExternalCache(context);
+        cleanDatabases(context);
+        cleanSharedPreference(context);
+        cleanFiles(context);
+        if (filepath == null) {
+            return;
+        }
+        for (String filePath : filepath) {
+            cleanCustomFile(filePath);
         }
     }
 
---
+++ app/src/main/java/crazy/zihao/androidutil/util/FileUtils.java
@@ -0,0 +1,144 @@
+package crazy.zihao.androidutil.util;
+
+import android.text.TextUtils;
+
+import java.io.File;
+import java.math.BigDecimal;
+
+/**
+ * ClassName：FileUtils
+ * Description：TODO<文件管理工具类>
+ * Author：Administrator
+ * Date：2017/7/17 10:04
+ * Email：jgduan@dohenes.com
+ * Version：v1.0
+ */
+public class FileUtils {
+
+    /**
+     * 删除方法 这里只会删除某个文件夹下的文件，如果传入的directory是个文件，将不做处理
+     *
+     * @param directory 目录
+     */
+    public static void deleteFilesByDirectory(File directory) {
+        if (directory.isFile()) {
+            directory.delete();
+        } else if (directory.exists()
+                && directory.isDirectory()) {
+            for (File item : directory.listFiles()) {
+                deleteFilesByDirectory(item);
+            }
+        }
+    }
+
+    /**
+     * 获取指定文件大小
+     *
+     * @param file 文件对象
+     * @return 存储空间size
+     * @throws Exception // 获取文件
+     *                   // Context.getExternalFilesDir() --> SDCard/Android/data/你的应用的包名/files/
+     *                   // 目录，一般放一些长时间保存的数据
+     *                   // Context.getExternalCacheDir() -->
+     *                   // SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
+     */
+    public static long getFolderSize(File file) throws Exception {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            for (int i = 0; i < fileList.length; i++) {
+                // 如果下面还有文件
+                if (fileList[i].isDirectory()) {
+                    size = size + getFolderSize(fileList[i]);
+                } else {
+                    size = size + fileList[i].length();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    /**
+     * 删除指定目录下文件及目录
+     *
+     * @param filePath       文件路径
+     * @param deleteThisPath 是否删除该路径文件或目录
+     */
+    public static void deleteFolderFile(String filePath, boolean deleteThisPath) {
+        if (!TextUtils.isEmpty(filePath)) {
+            try {
+                File file = new File(filePath);
+                if (file.isDirectory()) {// 如果下面还有文件
+                    File files[] = file.listFiles();
+                    for (File fl :
+                            files) {
+                        deleteFolderFile(fl.getAbsolutePath(), true);
+                    }
+                }
+
+                if (deleteThisPath) {
+                    if (!file.isDirectory()) {// 如果是文件，删除
+                        file.delete();
+                    } else {// 目录
+                        if (file.listFiles().length == 0) {// 目录下没有文件或者目录，删除
+                            file.delete();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 格式化单位
+     *
+     * @param size
+     * @return
+     */
+    public static String getFormatSize(double size) {
+        double kiloByte = size / 1024;
+        if (kiloByte < 1) {
+            return size + "Byte";
+        }
+
+        double megaByte = kiloByte / 1024;
+        if (megaByte < 1) {
+            BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
+            return result1.setScale(2, BigDecimal.ROUND_HALF_UP)
+                    .toPlainString() + "KB";
+        }
+
+        double gigaByte = megaByte / 1024;
+        if (gigaByte < 1) {
+            BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
+            return result2.setScale(2, BigDecimal.ROUND_HALF_UP)
+                    .toPlainString() + "MB";
+        }
+
+        double teraBytes = gigaByte / 1024;
+        if (teraBytes < 1) {
+            BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
+            return result3.setScale(2, BigDecimal.ROUND_HALF_UP)
+                    .toPlainString() + "GB";
+        }
+        BigDecimal result4 = new BigDecimal(teraBytes);
+        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString()
+                + "TB";
+    }
+
+    /**
+     * 获取指定文件占用缓存空间的大小
+     *
+     * @param file 文件对象
+     * @return 占据的缓存空间大小
+     * @throws Exception
+     */
+    public static String getCacheSize(File file) throws Exception {
+        return getFormatSize(getFolderSize(file));
+    }
+
+}
